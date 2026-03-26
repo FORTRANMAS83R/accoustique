ee517d8351e8373b78dbe69c4c8c730546d84bb2
Gargamel <mikael.franco@imt-atlantique.fr>
2026-03-26 10:17:54 +0100

    modif: fix constant name bug

--- sonar_equation.py
+++ sonar_equation.py
@@ -8,16 +8,16 @@ class SonarEquation:
         self.directivity_index = directivity_index_db
         self.detection_threshold = detection_threshold_db # This can be set based on the desired probability of detection and false alarm
 
-    def spherical_TL(self, range: float) -> float:
-        return 20 * np.log10(range)
+    def spherical_TL(self, distance: float) -> float:
+        return 20 * np.log10(distance)
     
-    def cylindrical_TL(self, range: float) -> float:
-        return 10 * np.log10(range)
+    def cylindrical_TL(self, distance: float) -> float:
+        return 10 * np.log10(distance)
     
-    def compute_SNR(self, range: np.ndarray, model: str = "spherical") -> np.ndarray:
+    def compute_SNR(self, distance: np.ndarray, model: str = "spherical") -> np.ndarray:
         if model == "spherical":
-            self.transmission_loss = self.spherical_TL(range)
+            self.transmission_loss = self.spherical_TL(distance)
         elif model == "cylindrical":
-            self.transmission_loss = self.cylindrical_TL(range)
+            self.transmission_loss = self.cylindrical_TL(distance)
         return self.source_level - self.transmission_loss + self.target_strength - self.noise_level + self.directivity_index
          
