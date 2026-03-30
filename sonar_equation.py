77b0dfd5a670712001f93bbf37c1dc1f7595aa86
Gargamel <mikael.franco@imt-atlantique.fr>
2026-03-30 19:35:24 +0200

    modify: the notebook (comments still need enhancing) + sonar_equation.py: add of methods

--- sonar_equation.py
+++ sonar_equation.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import francois_garisson as fg 
 class SonarEquation:
     def __init__(self, source_level_db: float, target_strength_db: float, noise_level_db: float, directivity_index_db: float, detection_threshold_db: float) -> float:
         self.source_level = source_level_db
@@ -13,11 +14,19 @@ class SonarEquation:
     
     def cylindrical_TL(self, distance: float) -> float:
         return 10 * np.log10(distance)
-    
-    def compute_SNR(self, distance: np.ndarray, model: str = "spherical") -> np.ndarray:
-        if model == "spherical":
+    def set_absorption_loss_const(self, c, S, T, z, f, pH = 7.5): 
+        self.c = c 
+        self.S = S 
+        self.T = T 
+        self.z = z 
+        self.f = f 
+        self.pH = pH
+    def compute_SNR(self, distance: np.ndarray, geometrical_model: str = "spherical", absorption_loss: bool = False) -> np.ndarray:
+        if geometrical_model == "spherical":
             self.transmission_loss = self.spherical_TL(distance)
-        elif model == "cylindrical":
+        elif geometrical_model == "cylindrical":
             self.transmission_loss = self.cylindrical_TL(distance)
+        if absorption_loss: 
+            self.transmission_loss += fg.compute_alpha(self.c, self.S, self.T, self.z, self.f, self.pH) * (distance * 10**(-3))
         return self.source_level - self.transmission_loss + self.target_strength - self.noise_level + self.directivity_index
          
